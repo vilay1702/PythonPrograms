67bcf8537b8f377f50ac4a87ad37ddd80828af9e
Vilay Bende <71513950+vilay1702@users.noreply.github.com>
2021-05-26 21:53:46 +0530

    Update ticTacToe.py

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,172 +1,182 @@
-import random
+import random 
 import math
 
-
 class Game:
-    # Initialising
     def __init__(self):
         self.board = [' ' for i in range(9)]
-        self.winner = None
-        self.turn = 'X'
-        self.isContinue = True
-        self.canMove = [i for i in range(1, 10)]
+        self.winner = None 
 
 
-    # start the game from here
-    def play(self, playerMode):
+    def __str__(self):
+        return str(''.join(self.board))
 
-        self.displayBoard()
-
-        while self.isContinue:
-            if playerMode == '1':
-                self.computerMove()
-            elif playerMode == '2':
-                self.makeMove()
-            self.isOver()
-            self.isTie()
-
-        if self.winner != None:
-            print('==================')
-            print('| ' + self.winner + ' PLAYER WON!! |')
-            print('==================')
-        else:
-            print('=================')
-            print('| GAME IS TIE!! |')
-            print('=================')
-
-
-    # Displays the board in tic tac toe format
+        
     def displayBoard(self):
-        print(self.board[0], '|', self.board[1], '|', self.board[2])
-        print('----------')
-        print(self.board[3], '|', self.board[4], '|', self.board[5])
-        print('----------')
-        print(self.board[6], '|', self.board[7], '|', self.board[8])
-
-
-    # Make moves by player and computer also, for single player game
-    def computerMove(self):
-
+        print()
+        print('\t' + self.board[0], '|', self.board[1], '|', self.board[2])
+        print('\t' + '--+---+--')
+        print('\t' + self.board[3], '|', self.board[4], '|', self.board[5])
+        print('\t' + '--+---+--')
+        print('\t' + self.board[6], '|', self.board[7], '|', self.board[8])
         print()
 
-        if self.turn == 'X':
-            print(self.turn + "'s Player Turn")
-            pos = input('Choose a position from 1 to 9: ')
-            try:
-                pos = int(pos)
-            except:
-                print('Please enter a valid position')     
 
-        else:
-            print("Computer's move")
-            pos = self.computer()
-
-        if pos <= 9 and self.isValid(pos-1):
-            self.board[pos-1] = self.turn
-            del self.canMove[self.canMove.index(pos)]
-            self.turn = 'X' if self.turn == 'O' else 'O'
-            self.displayBoard()
-            
-        else:
-            print('=================================')
-            print('| Please enter a valid position |')
-            print('=================================')    
-
-
-    # Computer chooses a place according to valid spaces
-    def computer(self):
-        pos = random.choice(self.canMove)
-        return pos
-
-
-    # function to make move by player, for multiplayer game
-    def makeMove(self):
-        # print('____________________________')
-        print()
-        print(self.turn + "'s Player Turn")
-        pos = input('Choose a position from 1 to 9: ')
-
-        try:
-            pos = int(pos) - 1
-        except:
-            print('Please enter a valid position')
-
-        if pos <= 9 and self.isValid(pos):
-            self.board[pos] = self.turn
-            self.turn = 'X' if self.turn == 'O' else 'O'
-            self.displayBoard()
-
-        else:
-            print('=================================')
-            print('| Please enter a valid position |')
-            print('=================================')
-
-    
-    # return True if selected position is empty
     def isValid(self, position):
         return self.board[position] == ' '
 
 
-    # Return True if any column, row or diagonal have same sign
-    def isOver(self):
-        if self.checkRow():
+    def makeMove(self, letter, position):
+        self.board[position] = letter
+
+
+    def isFull(self):
+        if (' ' in self.board):
+            return False
+        else:
             return True
-        if self.checkColumn():
-            return True
-        if self.checkDiagonal():
-            return True
+
+
+    def convertPosition(self, position):
+        return (position-1)
+    
+
+    # return True if any of the row have same sign
+    def checkRow(self, board):
+        for i in range(0, 9, 3):
+            if (board[i] == board[i+1]) and (board[i+1] == board[i+2]) and (board[i] == 'X' or board[i] == 'O') and (self.winner != None):
+                self.winner = board[i]
+                return True
         return False
 
 
-    # return True if any of the row have same sign
-    def checkRow(self):
-        for i in range(0, 9, 3):
-            if (self.board[i] == self.board[i+1]) and (self.board[i+1] == self.board[i+2]) and (self.board[i] == 'X' or self.board[i] == 'O'):
-                self.winner = self.board[i]
-                self.isContinue = False
-                return True
-
-
     # return True if any of the column have same sign
-    def checkColumn(self):
+    def checkColumn(self, board):
         for i in range(3):
-            if (self.board[i] == self.board[i+3]) and (self.board[i+3] == self.board[i+6]) and (self.board[i] == 'X' or self.board[i] == 'O'):
-                self.winner = self.board[i]
-                self.isContinue = False
+            if (board[i] == board[i+3]) and (board[i+3] == board[i+6]) and (board[i] == 'X' or board[i] == 'O') and (self.winner != None):
+                self.winner = board[i]
                 return True
+        return False
+
 
     # return True if either of diagonal have same sign
-    def checkDiagonal(self):
-        if (self.board[0] == self.board[4]) and (self.board[4] == self.board[8]) and (self.board[4] == 'X' or self.board[4] == 'O'):
-            self.winner = self.board[4]
-            self.isContinue = False
+    def checkDiagonal(self, board):
+        if (board[0] == board[4]) and (board[4] == board[8]) and (board[4] == 'X' or board[4] == 'O') and (self.winner != None):
+            self.winner = board[4]
+            return True 
+
+        if (board[2] == board[4]) and (board[4] == board[6]) and (board[4] == 'X' or board[4] == 'O') and (self.winner != None):
+            self.winner = board[4]
             return True
+        return False 
 
-        if (self.board[2] == self.board[4]) and (self.board[4] == self.board[6]) and (self.board[4] == 'X' or self.board[4] == 'O'):
-            self.winner = self.board[4]
-            self.isContinue = False
+
+    def isWinner(self, board):
+        if(self.checkRow(board) or self.checkColumn(board) or self.checkDiagonal(board)):
             return True
+        else:
+            return False 
 
 
-    # Return True is all places have filled
-    def isTie(self):
-        if not (' ' in self.board):
-            self.isContinue = False
+    def playerMove(self):
+        # print('____________________________')
+        while True:
+            print()
+            print("X's Player Turn")
+            pos = input('Choose a position from 1 to 9: ')
+
+            try:
+                pos = int(pos) 
+
+                pos = self.convertPosition(pos)
+
+                if ((pos>0) or (pos < 9)) and self.isValid(pos):
+                    self.makeMove('X', pos)
+                    # self.displayBoard()
+                    return 
+
+                else:
+                    print('+-------------------------------+')
+                    print('| Please enter a valid position |')
+                    print('+-------------------------------+')
+                    self.displayBoard()
+                
+            except:
+                print('+-------------------------------+')
+                print('| Please enter a valid position |')
+                print('+-------------------------------+')
+                self.displayBoard()
+
+    def computerMove(self):
+        move = -1
+        possibleMoves = [x for x, letter in enumerate(self.board) if letter == ' ']
+
+        for m in ['O', 'X']:
+            for i in possibleMoves:
+                boardCopy = self.board[:]
+                boardCopy[i] = m 
+                if self.isWinner(boardCopy):
+                    move = i 
+                    # return move 
+                    self.makeMove('O', move)
+                    return
+            corners = []    
+            for i in possibleMoves:
+                if i in [0, 2, 6, 8]:
+                    corners.append(i)
+
+            if len(corners) > 0:
+                move = random.choice(corners)
+                # return move 
+                self.makeMove('O', move)
+                return
+
+            if 5 in possibleMoves:
+                move = 5
+                # return move
+                self.makeMove('O', move)
+                return
+
+            edges = []
+            for i in [1, 3, 5, 7]:
+                edges.append(i)
+            
+            if len(edges) > 0:
+                move = random.choice(edges)
+                # return move
+                self.makeMove('O', move)
+                return
 
 
-# Starting the main program
-while True:
+    def play(self):
+        while True:
+            self.displayBoard()
+            self.playerMove()
+            self.computerMove()
+            print(self.board)
 
-    print("=============================")
-    print("Choose any of the following")
-    print("1 for Single Player")
-    print("2 for Multiplayer")
-    print("Any other key to exit")
-    playerMode = input("Choose: ")
+            print(self.isWinner(self.board))
+            if self.isWinner(self.board):
+                return
+            if not(' ' in self.board):
+                print('+---------------+')
+                print('| GAME IS TIE!! |')
+                print('+---------------+')
+                return 
+        
+        
 
-    if not (playerMode == '1' or playerMode == '2'):
-        print("================== Thankyou !! ==================")
-        break
 
-    game = Game()
-    game.play(playerMode)
+# =================== Main ===================
+
+print('+-------------------------------+')
+print('|            Welcome            |')
+print('+-------------------------------+') 
+
+isRematch = True
+while isRematch:
+    g = Game()
+    g.play()
+
+    userChoice = input("Enter 1 to rematch and any other key to exit: ")
+    if userChoice != "1":
+        isRematch = False
